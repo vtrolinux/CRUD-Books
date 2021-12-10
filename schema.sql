51becdc1333aa9da0ef19e4ddbd2f4884bb9129e
vtrolinux <vtrolinux@gmail.com>
2021-12-09 21:28:54 -0300

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pageqty` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
